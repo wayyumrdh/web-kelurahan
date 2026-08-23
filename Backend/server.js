1430108b51065b91f2559787ea8b37b2b42b48ad
wayyumrdh <wahyumardhatullah@gmail.com>
2026-08-24 05:26:23 +0800

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -34,6 +34,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 
+app.options('*', cors());
+
 // 3. PEMBATASAN UKURAN BODY
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
