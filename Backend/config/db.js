4a4ad9667c52cc5c98a0f2b6108915874206b512
wayyumrdh <wahyumardhatullah@gmail.com>
2026-08-24 05:35:27 +0800

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST || 'localhost',
-  user: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || '',
-  database: process.env.MYSQLDATABASE || 'db_kelurahan_mallawa',
-  port: process.env.MYSQLPORT || 3307,
+  host: 'mysql.railway.internal',
+  user: 'root',
+  password: 'etzLwtyzECSSGFqTiXujImSEdJtDFJwW',
+  database: 'railway',
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
