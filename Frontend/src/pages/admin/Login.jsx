6d373e3d059d0127b279fca56490858014d4bfd1
wayyumrdh <wahyumardhatullah@gmail.com>
2026-08-24 04:37:04 +0800

    Update Login.jsx

--- Frontend/src/pages/admin/Login.jsx
+++ Frontend/src/pages/admin/Login.jsx
@@ -23,7 +23,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('https://web-kelurahan-production.up.railway.app/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
