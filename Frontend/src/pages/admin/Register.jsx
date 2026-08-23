21682545b4e3df434e5f8a5ff50794388ccb319a
wayyumrdh <wahyumardhatullah@gmail.com>
2026-08-24 04:36:43 +0800

    Update Register.jsx

--- Frontend/src/pages/admin/Register.jsx
+++ Frontend/src/pages/admin/Register.jsx
@@ -27,7 +27,7 @@ export default function Register() {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/register', {
+      const response = await fetch('https://web-kelurahan-production.up.railway.app/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
